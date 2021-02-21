29a839f714a667d6b1f56c4115451bc9c1f45b7e
Tembeon <tembeon@gmail.com>
2021-02-21 14:10:33 +0300

    migrate to TextSelectionTheme

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -5,10 +5,10 @@ ThemeData themeData(BuildContext context) {
   //light theme
   return ThemeData(
     brightness: Brightness.light,
-    // ignore: deprecated_member_use
-    textSelectionColor: Color(0xFF777777),
-    // ignore: deprecated_member_use
-    textSelectionHandleColor: Color(0xFF000000),
+    textSelectionTheme: TextSelectionThemeData(
+      selectionColor: Color(0xFF777777),
+      selectionHandleColor: Color(0xFF000000),
+    ),
     primaryColor: Colors.white,
     accentColor: Colors.black,
     backgroundColor: Colors.white,
@@ -20,10 +20,10 @@ ThemeData darkThemeData(BuildContext context) {
   //dark theme
   return ThemeData.dark().copyWith(
     brightness: Brightness.dark,
-    // ignore: deprecated_member_use
-    textSelectionColor: Color(0xFF777777),
-    // ignore: deprecated_member_use
-    textSelectionHandleColor: Color(0xFFFFFFFF),
+    textSelectionTheme: TextSelectionThemeData(
+      selectionColor: Color(0xFF777777),
+      selectionHandleColor: Color(0xFFFFFFFF),
+    ),
     primaryColor: Color(0xFF222222),
     accentColor: Colors.white,
 
